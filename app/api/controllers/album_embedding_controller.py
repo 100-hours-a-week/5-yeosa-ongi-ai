b4efd4fee6b288cd621723725f1ba764ce7e638f
dahyun <gimd15629@gmail.com>
2025-05-01 15:43:29 +0900

    feat: get_image_loader() 활용하도록 수정

--- app/api/controllers/album_embedding_controller.py
+++ app/api/controllers/album_embedding_controller.py
@@ -1,4 +1,4 @@
-from app.utils.image_loader import load_images
+from app.utils.image_loader import get_image_loader
 from app.service.embedding import embed_images  # service import
 from app.schemas.album_schema import ImageRequest
 from fastapi import Request
@@ -6,7 +6,9 @@ from fastapi import Request
 
 def embed_controller(req: ImageRequest, request: Request):
     filenames = req.images
-    images = load_images(filenames)
+
+    image_loader = get_image_loader()
+    images = image_loader.load_images(filenames)
 
     clip_model = request.app.state.clip_model
     clip_preprocess = request.app.state.clip_preprocess
--- app/api/controllers/album_people_controller.py
+++ app/api/controllers/album_people_controller.py
@@ -1,4 +1,4 @@
-from app.utils.image_loader import load_images
+from app.utils.image_loader import get_image_loader
 from app.schemas.album_schema import ImageRequest
 from app.service.people import cluster_faces
 from fastapi import Request
@@ -6,7 +6,9 @@ from fastapi import Request
 
 def people_controller(req: ImageRequest, request: Request):
     filenames = req.images
-    images = load_images(filenames)
+    
+    image_loader = get_image_loader()
+    images = image_loader.load_images(filenames)
 
     arcface_model = request.app.state.arcface_model
     yolo_detector = request.app.state.yolo_detector
