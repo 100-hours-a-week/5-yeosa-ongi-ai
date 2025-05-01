2caaaa5a9939877dbfcc2c4d752d02a52ac70f5a
dahyun <gimd15629@gmail.com>
2025-05-01 15:35:48 +0900

    feat: 이미지 로딩 추상 클래스 방식 구현

--- app/utils/image_loader.py
+++ app/utils/image_loader.py
@@ -1,11 +1,18 @@
 import os
 from PIL import Image
+from abc import ABC, abstractmethod
 
+class BaseImageLoader(ABC):
+    @abstractmethod
+    def load_images(self) -> list[Image.Image]:
+        pass
 
-def load_images(filenames: list[str]) -> list[Image.Image]:
-    images_path = "Users/images"
-    images = [
-        Image.open(os.path.join(images_path, filename)).convert("RGB")
-        for filename in filenames
-    ]
-    return images
+class LocalImageLoader(BaseImageLoader):
+    def __init__(self, image_dir: str = "Users/images"):
+        self.image_dir = image_dir 
+
+    def load_images(self, filenames: list[str]) -> list[Image.Image]:
+        return [
+            Image.open(os.path.join(self.image_dir, filename)).convert("RGB")
+            for filename in filenames
+        ]
