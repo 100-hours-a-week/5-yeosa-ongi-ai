0dbf6b47fa6ada9e8dbf81a9539e630779a2d085
이상원 <simon.lee@isang-won-ui-MacBookPro.local>
2025-05-12 16:16:21 +0900

    refactor: 모델에 따라 동적으로 받아올 수 있게 aesthetic_regressor.py 업데이트

--- app/model/aesthetic_regressor.py
+++ app/model/aesthetic_regressor.py
@@ -2,12 +2,13 @@ import torch
 
 _aesthetic_regressor = None
 
+dim = {'ViT-B/32': 512, 'ViT-L/14': 768}
 
 class AestheticRegressor(torch.nn.Module):
     def __init__(self, model_name='ViT-B/32'):
         super().__init__()
-        self.fc = torch.nn.Linear(512 if model_name == 'ViT-B/32' else 768, 1)
-        self.load_state_dict(torch.load(f"app/model/aesthetic_regressor_{'_'.join(model_name.split('/'))}.pth"))
+        self.fc = torch.nn.Linear(dim[model_name], 1)
+        self.load_state_dict(torch.load(f"app/model/{model_name}/aesthetic_regressor.pth"))
 
     def forward(self, x):
         return self.fc(x).squeeze(1)
