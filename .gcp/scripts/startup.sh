199146b4e57e08f9b9cc1438d963b0b368048415
Kim Dahyun <ganghwapeople@gmail.com>
2025-06-22 11:07:55 +0900

    chore: startup.sh에 환경변수 주입 추가

--- .gcp/scripts/startup.sh
+++ .gcp/scripts/startup.sh
@@ -3,9 +3,11 @@ set -e
 
 export IMAGE_TAG=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/IMAGE_TAG)
 export PROJECT_ID=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/PROJECT_ID)
+export APP_ENV=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/APP_ENV)
 
 echo "[INFO] IMAGE_TAG=${IMAGE_TAG}"
 echo "[INFO] PROJECT_ID=${PROJECT_ID}"
+echo "[INFO] APP_ENV=${APP_ENV}"
 
 echo "Docker 설치"
 apt-get update
@@ -40,6 +42,7 @@ docker pull asia-northeast3-docker.pkg.dev/dev-ongi-3-tier/dev-ongi-ai-repo/ai-c
 echo "FastAPI 앱 컨테이너 실행"
 docker run -d \
   --name ai-cpu \
+  -e APP_ENV="$APP_ENV" \  
   -e PROJECT_ID="$PROJECT_ID" \
   -p 8000:8000 \
   --restart unless-stopped \
