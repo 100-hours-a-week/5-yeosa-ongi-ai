a234d8732c4448c2638eeed76ffb25d0e7a1e56e
Kim Dahyun <ganghwapeople@gmail.com>
2025-07-03 16:33:54 +0900

    feat: kafka 도입

--- app/main.py
+++ app/main.py
@@ -16,12 +16,15 @@ os.environ["JOBLIB_NUM_THREADS"] = "1"
 
 import torch
 from fastapi import FastAPI
+from aiokafka.errors import KafkaConnectionError
 
 from app.config.secret_loader import load_secrets_from_gcp
-load_secrets_from_gcp()
+# load_secrets_from_gcp()
 
 from app.api import api_router
-from app.config.redis import init_redis
+from app.kafka.consumer import create_kafka_consumer, consume_loop
+from app.kafka.producer import create_kafka_producer
+from app.config.app_config import get_config
 from app.config.settings import IMAGE_MODE, MODEL_NAME, MODEL_BASE_PATH, CATEGORY_FEATURES_FILENAME, QUALITY_FEATURES_FILENAME
 from app.middleware.error_handler import setup_exception_handler
 from app.model.aesthetic_regressor import load_aesthetic_regressor
@@ -40,61 +43,41 @@ if not GPU_SERVER_BASE_URL:
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """서버 실행 시, 모델 및 이미지 로더 초기화 로직입니다."""
-    aesthetic_regressor = load_aesthetic_regressor(MODEL_NAME)
-    loop = asyncio.get_running_loop()
-    executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
-    loop.set_default_executor(executor)
-    category_data = torch.load(os.path.join(MODEL_BASE_PATH, CATEGORY_FEATURES_FILENAME), weights_only=False)
-    parent_categories = category_data["parent_categories"]
-    parent_embeds = category_data["parent_embeds"]
-    embed_dict = category_data["embed_dict"]
-    category_dict = category_data["category_dict"]
-    quality_data = torch.load(os.path.join(MODEL_BASE_PATH, QUALITY_FEATURES_FILENAME), weights_only=True)
-    quality_text_features = quality_data["text_features"]
-    quality_fields = quality_data["fields"]
+    config = get_config()
+    await config.initialize()
 
-    app.state.aesthetic_regressor = aesthetic_regressor
-    app.state.image_loader = get_image_loader(IMAGE_MODE)
-    app.state.loop = loop
-    app.state.parent_categories = parent_categories
-    app.state.parent_embeds = parent_embeds
-    app.state.embed_dict = embed_dict
-    app.state.category_dict = category_dict
-    app.state.quality_text_features = quality_text_features
-    app.state.quality_fields = quality_fields
-    app.state.redis = init_redis()
-    app.state.gpu_client = httpx.AsyncClient(
-        base_url=GPU_SERVER_BASE_URL,
-        timeout=60.0,
-        headers={"Content-Type": "application/json"},
-    )
+    consumer = create_kafka_consumer(bootstrap_servers=config.kafka_bootstrap_servers)
+    producer = create_kafka_producer(bootstrap_servers=config.kafka_bootstrap_servers)
 
     try:
-        pong = await app.state.redis.ping()
-        if pong:
-            print("Redis 연결 성공")
-    except Exception as e:
-        print(f"Redis 연결 실패: {e}")
+        await consumer.start()
+        print(f"kafka Consumer 연결 성공")
+    except KafkaConnectionError as e:
+        print(f"kafka Consumer 연결 실패: {e}")
+        
+    try:
+        await producer.start()
+        print(f"kafka Producer 연결 성공")
+    except KafkaConnectionError as e:
+        print(f"kafka Producer 연결 실패: {e}")
 
-    if IMAGE_MODE == IMAGE_MODE.S3:
-        if isinstance(app.state.image_loader, S3ImageLoader):
-            await app.state.image_loader.init_client()
 
-    yield
+    # Kafka 백그라운드 태스크 실행
+    kafka_consumer_task = asyncio.create_task(consume_loop(consumer, producer))
 
-    # 서버 종료 시 리소스 해제
-    if IMAGE_MODE == IMAGE_MODE.GCS:
-        if isinstance(app.state.image_loader, GCSImageLoader):
-            await app.state.image_loader.client.close()
-
-            temp_path = getattr(app.state.image_loader, "_temp_file_path", None)
-            if temp_path and os.path.exists(temp_path):
-                os.remove(temp_path)
-                print(f"임시 GCP 키 파일 삭제됨: {temp_path}")
-
-    if IMAGE_MODE == IMAGE_MODE.S3:
-        if isinstance(app.state.image_loader, S3ImageLoader):
-            await app.state.image_loader.close_client()
+    try:
+        yield
+    finally:
+        """서버 종료 시, 리소스 해제 로직입니다."""
+        await config.cleanup()
+        await consumer.stop()
+        await producer.stop()
+        kafka_consumer_task.cancel()
+        try:
+            await kafka_consumer_task
+        except asyncio.CancelledError:
+            pass
+    
 
 app = FastAPI(lifespan=lifespan)
 torch.set_num_threads(1)
