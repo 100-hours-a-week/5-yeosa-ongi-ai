71fbb21e6c47a7ab587c6120167889f666435bef
Kim Dahyun <ganghwapeople@gmail.com>
2025-07-07 20:33:30 +0900

    refact: 컨슈머 그룹 환경 변수 등록

--- app/config/kafka_config.py
+++ app/config/kafka_config.py
@@ -1,18 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# 로컬 개발 시 .env 로드
 load_dotenv()
-
 
 KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
 KAFKA_GROUP_ID = os.getenv("KAFKA_GROUP_ID")
+KAFKA_GPU_GROUP_ID = os.getenv("KAFKA_GPU_GROUP_ID")
 
-if KAFKA_BROKER_URL is None:
+if not KAFKA_BROKER_URL:
     raise EnvironmentError("KAFKA_BROKER_URL를 설정해야 합니다.")
-if KAFKA_GROUP_ID is None:
-    raise EnvironmentError("KAFKA_GROUP_ID를 설정해야 합니다.")
-
+if not KAFKA_GROUP_ID or not KAFKA_GPU_GROUP_ID:
+    raise EnvironmentError("KAFKA 그룹 ID를 모두 설정해야 합니다.")
 
 KAFKA_REQUEST_TOPICS = [
     "album.ai.embedding.request",
@@ -20,16 +18,7 @@ KAFKA_REQUEST_TOPICS = [
     "album.ai.quality.request",
     "album.ai.category.request",
     "album.ai.score.request",
-    "album.ai.people.request"
-]
-
-KAFKA_RESPONSE_TOPICS = [
-    "album.ai.embedding.response",
-    "album.ai.duplicate.response",
-    "album.ai.quality.response",
-    "album.ai.category.response",
-    "album.ai.score.response",
-    "album.ai.people.response"
+    "album.ai.people.request",
 ]
 
 KAFKA_RESPONSE_TOPIC_MAP = {
